7f768fd453afe67b625b0941ae3f568c8cf55782
West Amta <westamta@gmail.com>
2024-07-02 09:46:04 +0530

    Yearwise Teacher Nav Added

--- src/app/YearWiseTeachers/page.jsx
+++ src/app/YearWiseTeachers/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { useGlobalContext } from "../context/Store";
+import { useGlobalContext } from "../../context/Store";
 import { useRouter } from "next/navigation";
 import {
   getServiceLife,
