61071e7638ad7cad740ce4f44ad3a16bb4b3dc49
ultimate365 <maidul365@gmail.com>
2025-09-11 09:26:56 +0530

    Modified

--- src/app/api/updteacher/route.ts
+++ src/app/api/updteacher/route.ts
@@ -10,13 +10,11 @@ export async function POST(request: NextRequest) {
       school,
       udise,
       tname,
-      tsname,
       gender,
-      ph,
+      disability,
       desig,
       fname,
       circle,
-      sis,
       gp,
       association,
       phone,
@@ -32,30 +30,8 @@ export async function POST(request: NextRequest) {
       training,
       pan,
       address,
-      basic,
-      mbasic,
-      prevmbasic,
-      addl,
-      da,
-      mda,
-      hra,
-      mhra,
-      ma,
-      gross,
-      mgross,
-      gpf,
-      gpfprev,
-      julyGpf,
-      mptax,
-      jptax,
-      gsli,
-      netpay,
-      mnetpay,
-      bonus,
-      arrear,
       question,
       hoi,
-      newHt,
       showAccount,
       service,
       id,
@@ -69,13 +45,10 @@ export async function POST(request: NextRequest) {
       teacherData.school = school;
       teacherData.udise = udise;
       teacherData.tname = tname;
-      teacherData.tsname = tsname;
       teacherData.gender = gender;
-      teacherData.ph = ph;
       teacherData.desig = desig;
       teacherData.fname = fname;
       teacherData.circle = circle;
-      teacherData.sis = sis;
       teacherData.gp = gp;
       teacherData.association = association;
       teacherData.phone = phone;
@@ -91,30 +64,8 @@ export async function POST(request: NextRequest) {
       teacherData.training = training;
       teacherData.pan = pan;
       teacherData.address = address;
-      teacherData.basic = basic;
-      teacherData.mbasic = mbasic;
-      teacherData.prevmbasic = prevmbasic;
-      teacherData.addl = addl;
-      teacherData.da = da;
-      teacherData.mda = mda;
-      teacherData.hra = hra;
-      teacherData.mhra = mhra;
-      teacherData.ma = ma;
-      teacherData.gross = gross;
-      teacherData.mgross = mgross;
-      teacherData.gpf = gpf;
-      teacherData.gpfprev = gpfprev;
-      teacherData.julyGpf = julyGpf;
-      teacherData.mptax = mptax;
-      teacherData.jptax = jptax;
-      teacherData.gsli = gsli;
-      teacherData.netpay = netpay;
-      teacherData.mnetpay = mnetpay;
-      teacherData.bonus = bonus;
-      teacherData.arrear = arrear;
       teacherData.question = question;
       teacherData.hoi = hoi;
-      teacherData.newHt = newHt;
       teacherData.showAccount = showAccount;
       teacherData.service = service;
       teacherData.id = id;
@@ -125,12 +76,10 @@ export async function POST(request: NextRequest) {
       let userData = await User.findOne({ id: id });
       if (userData) {
         userData.tname = tname;
-        userData.tsname = tsname;
         userData.school = school;
         userData.desig = desig;
         userData.pan = pan;
         userData.udise = udise;
-        userData.sis = sis;
         userData.circle = circle;
         userData.empid = empid;
         userData.question = question;
